660622d483aeb9eeac40745eda545f01299e3d92
Pierfrancesco Soffritti <soffritti.pierfrancesco@gmail.com>
2016-11-06 13:00:48 +0100

    changed default waveform

--- 4. SoUNds/js/SoundGenerator.js
+++ 4. SoUNds/js/SoundGenerator.js
@@ -12,7 +12,7 @@ function SoundGenerator() {
     analyser.smoothingTimeConstant = 0.7;
     analyser.fftSize = this.fftSize;
 
-    var waveForm = "triangle";
+    var waveForm = "square";
     var fact = 6;
 
     $("body").keypress(event => {
