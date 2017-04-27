6e763526a582b62fc0ae08c4edf0eccdf8bd11a6
Pierfrancesco Soffritti <soffritti.pierfrancesco@gmail.com>
2017-04-27 10:11:22 +0200

    disabled fps counter

--- 13. ProceduralTerrain/js/main.js
+++ 13. ProceduralTerrain/js/main.js
@@ -2,7 +2,7 @@ var canvas = document.getElementById("canvas");
 
 window.onresize = resizeCanvas;
 
-(function(){var script=document.createElement('script');script.onload=function(){var stats=new Stats();document.body.appendChild(stats.dom);requestAnimationFrame(function loop(){stats.update();requestAnimationFrame(loop)});};script.src='//rawgit.com/mrdoob/stats.js/master/build/stats.min.js';document.head.appendChild(script);})()
+// (function(){var script=document.createElement('script');script.onload=function(){var stats=new Stats();document.body.appendChild(stats.dom);requestAnimationFrame(function loop(){stats.update();requestAnimationFrame(loop)});};script.src='//rawgit.com/mrdoob/stats.js/master/build/stats.min.js';document.head.appendChild(script);})()
 
 resizeCanvas();
 
