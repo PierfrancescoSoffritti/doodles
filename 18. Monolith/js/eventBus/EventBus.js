8b0bee9c9a348429e29052fd8ac82438ffa59890
Pierfrancesco Soffritti <soffritti.pierfrancesco@gmail.com>
2018-02-18 21:56:37 +0100

    refactoring event bus

--- 18. Monolith/js/eventBus/EventBus.js
+++ 18. Monolith/js/eventBus/EventBus.js
@@ -1,45 +1,32 @@
 function EventBus() {
     const eventObjects = new Array();
     
-    /**
-     * [subscribe Subscribe for a specific event]
-     * @param  {[string]}   eventType [the event name]
-     * @param  {Function} callback  [function that will be called when 'eventType' occurs]
-     */
-    this.subscribe = function (eventType, callback) {
-        // check if someone is already subscribed to this event
-        var eventObject = eventObjects.filter(function (eventObject) { return eventObject.eventType === eventType; })[0];
+    this.subscribe = function(eventType, callback) {
+        const eventObject = findEventObject(eventType)
 
-        if(eventObject) {
-            // if there are already callbacks for this event -> add the new callback
+        if(eventObject)
             eventObject.callbacks.push(callback);
-        } else {
-            // if there are no callbacks for this event -> create a new entry in the array
-            var eventObject = new EventObject(eventType);
-            eventObject.callbacks.push(callback);
+        else
+            eventObjects.push( new EventObject(eventType, callback) );
+    }
 
-            eventObjects.push(eventObject);
+    this.post = function(eventType, argument1, argument2) {
+        const eventObject = findEventObject(eventType)
+        
+        if(!eventObject) {
+            console.error("no subscribers for event " +eventType)
+            return;
         }
+
+        eventObject.callbacks.forEach( callback => callback(argument1, argument2) );
     }
 
-    /**
-     * [post post an envent on the event bus]
-     * @param  {[string]} eventType [event]
-     * @param  {[object]} argument  [an optional argument]
-     */
-    this.post = function (eventType, argument1, argument2) {
-        // call every callback of the event
-        eventObjects
-            .filter(function (eventObject) { return eventObject.eventType === eventType; })
-            .forEach(function (eventObject) { eventObject.callbacks.forEach(function (callback) { callback(argument1, argument2) }) });
+    function findEventObject(eventType) {
+        return eventObjects.find( eventObject => eventObject.eventType === eventType );
     }
 
-    /**
-     * [EventObject Class representing an association between an event type and its list of callbacks]
-     * @param {[string]} type [event name]
-     */
-    function EventObject(type) {
+    function EventObject(type, callback) {
         this.eventType = type;
-        this.callbacks = new Array();
+        this.callbacks = new Array(callback);
     }
 }
