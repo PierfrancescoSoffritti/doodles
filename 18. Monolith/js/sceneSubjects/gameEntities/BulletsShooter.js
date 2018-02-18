86562d494281529b61b57ff8231dd0e2bf41add3
Pierfrancesco Soffritti <soffritti.pierfrancesco@gmail.com>
2018-02-18 22:07:50 +0100

    refactoring

--- 18. Monolith/js/sceneSubjects/gameEntities/BulletsShooter.js
+++ 18. Monolith/js/sceneSubjects/gameEntities/BulletsShooter.js
@@ -5,7 +5,6 @@ function BulletsShooter(scene) {
 
     let iTime = 0
     const delay = .1
-    // const delay = .08
     let lastShootTime = 0
 
     this.shoot = function(originPosition) {
@@ -24,5 +23,38 @@ function BulletsShooter(scene) {
             if(expired)
                 bullets.splice(i, 1);
         }
+    }
+}
+
+const geometryBullet = new THREE.SphereBufferGeometry( .4, 16, 16 );
+const materialBullet = new THREE.MeshBasicMaterial( {color: "#0000FF"} );
+const blueprintBullet = new THREE.Mesh( geometryBullet, materialBullet );
+
+function Bullet(scene, originPosition) {
+
+    const sphere = blueprintBullet.clone()
+    sphere.scale.set(getRandom(.1, 1), getRandom(.1, 1), getRandom(.1, 1))
+
+    const speed = 5;
+
+    this.position = sphere.position
+    this.collision = false
+
+    sphere.position.set(originPosition.x, originPosition.y, originPosition.z)
+    scene.add(sphere);
+
+    const polarCoord = cartesianToPolar(originPosition.x, originPosition.z)
+    
+    this.update = function(time) {
+        polarCoord.radius -= speed
+
+        sphere.position.x = (polarCoord.radius) * cos(polarCoord.angle)
+        sphere.position.z = (polarCoord.radius) * sin(polarCoord.angle)
+
+        const expired = ( polarCoord.radius < 0 || this.collision === true ) ? true : false
+        if(expired)
+            scene.remove(sphere)
+            
+        return expired
     }
 }
--- 18. Monolith/js/sceneSubjects/gameEntities/EnemiesSpawner.js
+++ 18. Monolith/js/sceneSubjects/gameEntities/EnemiesSpawner.js
@@ -1,47 +1,54 @@
 function EnemiesSpawner(scene, gameConstants) {
     const enemies = []
+
     this.enemies = enemies
 
     const delay = .4
     let lastEnemySpawnTime = 0
 
-    const geometry = new THREE.BoxBufferGeometry( 2, 8, 4 )
-    const material = new THREE.MeshStandardMaterial( {color: 0x00ff00} )
-    const cube = new THREE.Mesh( geometry, material )
-    scene.add( cube )
+    const spawnerGeometry = new THREE.BoxBufferGeometry( 2, 8, 4 )
+    const spawnerMaterial = new THREE.MeshStandardMaterial( {color: 0x00ff00} )
+    const spawnerMesh = new THREE.Mesh( spawnerGeometry, spawnerMaterial )
+    scene.add( spawnerMesh )
     
+    spawnerMesh.position.y = gameConstants.baseLevelHeight    
+
     const speed = 1.2
 
     let lastEleveationChangeTime = 0
-    let changeElevationDelay = getRandom(2, 4)
+    let changeElevationDelay = 0
 
     this.update = function(time) {
-        updateSpawnerPosition(time)
+        updateSpawnerPolarPosition(time)
+        updateSpawnerHeight(time)
         spawnEnemy(time)
         updateEnemies(time)
     }
 
-    function updateSpawnerPosition(time) {
+    function updateSpawnerPolarPosition(time) {
         const agle = time*speed
         const position = polarToCartesian(gameConstants.minRadius, agle)
-        cube.position.x = position.x
-        cube.position.z = position.y
+        spawnerMesh.position.x = position.x
+        spawnerMesh.position.z = position.y
         
-        cube.rotation.y = -agle
+        spawnerMesh.rotation.y = -agle
+    }
 
-        if(time > lastEleveationChangeTime + changeElevationDelay) {
-            cube.position.y = cube.position.y === gameConstants.baseLevelHeight ? gameConstants.secondLevelHeight : gameConstants.baseLevelHeight
-            
-            lastEleveationChangeTime = time
-            changeElevationDelay = getRandom(2, 4)
-        }
+    function updateSpawnerHeight(time) {
+        if(time <= lastEleveationChangeTime + changeElevationDelay)
+            return
+
+        spawnerMesh.position.y = getRandom(0, 1) > 0.5 ? gameConstants.secondLevelHeight : gameConstants.baseLevelHeight
+        
+        lastEleveationChangeTime = time
+        changeElevationDelay = getRandom(2, 4)
     }
 
     function spawnEnemy(currentTime) {
         if(currentTime - lastEnemySpawnTime < delay)
             return
         
-        enemies.push(new Enemy(scene, gameConstants, cube.position))
+        enemies.push(new Enemy(scene, gameConstants, spawnerMesh.position))
         lastEnemySpawnTime = currentTime
     }
 
@@ -49,10 +56,13 @@ function EnemiesSpawner(scene, gameConstants) {
         for(let i=0; i<enemies.length; i++) {
             const expired = enemies[i].update(time)
 
-            if(expired) {
-                enemies.splice(i, 1)
-                eventBus.post(decreaseScore)
-            }
+            if(expired)
+                removeEnemy(i)
         }
+    }
+
+    function removeEnemy(i) {
+        enemies.splice(i, 1)
+        eventBus.post(decreaseScore)
     }
 }
